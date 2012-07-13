74b7116fdc35fd1cc6d30a7828e06f1fa240db01
David Soergel <dev@davidsoergel.com>
2012-07-13 11:29:47 -0400

    Add sbt-assembly plugin

--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -1,21 +1,20 @@
 import sbt._
 import sbt.Keys._
 
-object IeslPluginLoader extends Build {
+object IeslPluginLoader extends Build
+	{
 
-  // automatically updating SNAPSHOTs does not seem to work right here.
-  // as a workaround, we'll just release every incremental change to iesl-sbt-base with integer build numbers.
-
-  lazy val root = Project(id = "plugins", base = file("."))
-    .settings(resolvers += "IESL Public Releases" at "https://dev-iesl.cs.umass.edu/nexus/content/groups/public")
-    //.settings(resolvers += "IESL Public Snapshots" at "https://dev-iesl.cs.umass.edu/nexus/content/groups/public-snapshots")
-    .settings(addSbtPlugin("edu.umass.cs.iesl" %% "iesl-sbt-base" % "latest.release")) // apparently buggy: "latest.integration" changing()
-}
-
+	// automatically updating SNAPSHOTs does not seem to work right here.
+	// as a workaround, we'll just release every incremental change to iesl-sbt-base with integer build numbers.
+	lazy val root = Project(id = "plugins", base = file("."))
+	                .settings(resolvers += "IESL Public Releases" at "https://dev-iesl.cs.umass.edu/nexus/content/groups/public")
+	                //.settings(resolvers += "IESL Public Snapshots" at "https://dev-iesl.cs.umass.edu/nexus/content/groups/public-snapshots")
+	                .settings(addSbtPlugin("edu.umass.cs.iesl" %% "iesl-sbt-base" % "latest.release")) // apparently buggy: "latest.integration" changing()
+	                .settings(addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.8.3"))
+	}
 
 // as of sbt 0.12.0 we can rebuild the plugin on the fly from the hg repository,
 // avoiding the Nexus URL chicken-and-egg problem (or rather, pushing it back one level to the Bitbucket URL)
-
 /*
 import sbt._
 
