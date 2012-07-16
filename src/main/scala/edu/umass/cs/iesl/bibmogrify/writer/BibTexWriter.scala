978cd905465e9db25615b5634c07582087e4565a
David Soergel <dev@davidsoergel.com>
2012-07-16 16:00:29 -0400

    allow writing bibtex with empty authors

--- src/main/scala/edu/umass/cs/iesl/bibmogrify/writer/BibTexWriter.scala
+++ src/main/scala/edu/umass/cs/iesl/bibmogrify/writer/BibTexWriter.scala
@@ -6,54 +6,59 @@ import edu.umass.cs.iesl.bibmogrify.NamedPlugin
 import edu.umass.cs.iesl.bibmogrify.model._
 
 import RichCitationMention._
-import edu.umass.cs.iesl.scalacommons.{StringUtils, NonemptyString}
+import edu.umass.cs.iesl.scalacommons.NonemptyString
+import edu.umass.cs.iesl.scalacommons.StringUtils._
 
 /**
  * just writes the fields needed for Rexa2 import for now
  */
-object BibTexWriter extends Transformer[StructuredCitation, String] with NamedPlugin {
+object BibTexWriter extends Transformer[StructuredCitation, String] with NamedPlugin
+	{
 
-// ** add collection open and close tag
+	// ** add collection open and close tag
+	val name = "bibtex"
 
-  val name = "bibtex"
+	def apply(cm: StructuredCitation) =
+		{
+		val opener = cm.doctype.map(_ match
+		                            {
+			                            case JournalArticle => "@article {"
+			                            case ProceedingsArticle => "@inproceedings {"
+			                            case Proceedings => "@proceedings {"
+			                            case BookChapter => "@inbook {"
+			                            case Book => "@book {"
+			                            case TechnicalReport => "@technicalreport {"
+			                            case PhdThesis => "@phdthesis {"
+			                            case MastersThesis => "@mastersthesis {"
 
-  def apply(cm: StructuredCitation) = {
-    val opener = cm.doctype.map(
-    _ match {
-      case JournalArticle => "@article {"
-      case ProceedingsArticle => "@inproceedings {"
-      case Proceedings => "@proceedings {"
-      case BookChapter => "@inbook {"
-      case Book => "@book {"
-      case TechnicalReport => "@technicalreport {"
-      case PhdThesis => "@phdthesis {"
-      case MastersThesis => "@mastersthesis {"
 
+			                            // following may be nonstandard
+			                            case Patent => "@patent {"
+			                            case Grant => "@grant {"
+			                            case WwwArticle => "@wwwarticle {"
+			                            case Journal => "@journal {"
+			                            case CollectionArticle => "@collection {"
+			                            case CollectionOfArticles => "@incollection {"
+		                            }).getOrElse("@article {")
 
-      // following may be nonstandard
-      case Patent => "@patent {"
-      case Grant => "@grant {"
-      case WwwArticle => "@wwwarticle {"
-      case Journal => "@journal {"
-      case CollectionArticle => "@collection {"
-      case CollectionOfArticles => "@incollection {"
+		val id: String = cm.primaryId
+		val title: Option[(String, NonemptyString)] = cm.title.map(("title", _))
 
-    }).getOrElse("@article {")
+		// no need to filter author roles, because editors etc. are otherContributors
+		val authors: Option[(String, NonemptyString)] =
+			{
+			val s = cm.authors.map(_.agent).mkString(" and ")
+			emptyStringToNone(s).map(Some("author", _))
+			}
+		val year: Option[(String, NonemptyString)] = cm.dates.filter(_.eventType == Published).flatMap(_.date.flatMap(_.year)).headOption
+		                                             .map(y => ("year", NonemptyString(y.toString)))
+		val venue: Option[(String, NonemptyString)] = cm.containedIn.flatMap(_.container.title).map(("journal", _))
 
-    val id : String = cm.primaryId
-    val title: Option[(String, NonemptyString)] = cm.title.map(("title", _))
+		val closer = "},\n"
 
-    // no need to filter author roles, because editors etc. are otherContributors
-    val authors: Option[(String, NonemptyString)] = Some("author", NonemptyString(cm.authors.map(_.agent).mkString(" and ")))
-    val year: Option[(String, NonemptyString)] = cm.dates.filter(_.eventType == Published).flatMap(_.date.flatMap(_.year)).headOption.map(y => ("year",
-		    NonemptyString(y.toString)))
-    val venue: Option[(String, NonemptyString)] = cm.containedIn.flatMap(_.container.title).map(("journal", _))
-
-    val closer = "},\n"
-
-    val fields: Seq[(String, NonemptyString)] = Seq(title, authors, year, venue).flatten
-    val fieldsString = fields.map(x => "\t" + x._1 + " = {" + x._2.s.replace("{","\\{").replace("}","\\}") + "}").mkString(",\n")
-    val result = opener + id + ",\n" + fieldsString + closer
-    Some(result)
-  }
-}
+		val fields: Seq[(String, NonemptyString)] = Seq(title, authors, year, venue).flatten
+		val fieldsString = fields.map(x => "\t" + x._1 + " = {" + x._2.s.replace("{", "\\{").replace("}", "\\}") + "}").mkString(",\n")
+		val result = opener + id + ",\n" + fieldsString + closer
+		Some(result)
+		}
+	}
