54f7d225b418b8b3dca4f06fd57105d48c60d7a2
David Soergel <dev@davidsoergel.com>
2012-08-31 12:43:47 -0400

    update sbt-assembly and fix mainClass

--- project/Build.scala
+++ project/Build.scala
@@ -3,11 +3,12 @@ import sbtassembly.Plugin._
 
 import edu.umass.cs.iesl.sbtbase.{Dependencies, IeslProject}
 import edu.umass.cs.iesl.sbtbase.IeslProject._
+import AssemblyKeys._
+import Keys._
 
 // todo: move assembly stuff to iesl-sbt-base
 // this is just an example, to show how simple a build can be once all the boilerplate stuff is factored out.
-object BibmogrifyBuild extends Build
-	{
+object BibmogrifyBuild extends Build {
 
 	val vers = "0.1-SNAPSHOT"
 
@@ -15,74 +16,13 @@ object BibmogrifyBuild extends Build
 
 	import allDeps._
 
-	val deps = Seq(ieslScalaCommons("latest.integration"), namejuggler("latest.integration"), liftJson(), scalatest(), subcut(), langdetect(), jsonic(), commonsVfs2(),
-	               commonsCollections(),
-	               commonsCompress(),
+	val deps = Seq(ieslScalaCommons("latest.integration"), namejuggler("latest.integration"), liftJson(), scalatest(), subcut(), langdetect(), jsonic(),
+	               commonsVfs2(), commonsCollections(), commonsCompress(),
 	               // these should be provided transitively by scalacommons, but they aren't because it's defined "notTransitive"
-	               dsutils(), commonsLang(), classutil(),"com.mongodb.casbah" % "casbah_2.9.0-1" % "2.1.5.0")
+	               dsutils(), commonsLang(), classutil(), "com.mongodb.casbah" % "casbah_2.9.0-1" % "2.1.5.0")
 
 	lazy val bibmogrify = IeslProject("bibmogrify", vers, deps, Public, WithSnapshotDependencies).settings(addCompilerPlugin(subcut()))
-	                      .settings(assemblySettings: _*).settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*).cleanLogging.standardLogging
+	                      .settings(assemblySettings: _*).settings(mainClass in assembly := Some("edu.umass.cs.iesl.bibmogrify.BibMogrify"))
+	                      .settings(net.virtualvoid.sbt.graph.Plugin.graphSettings: _*).cleanLogging.standardLogging
 
-	/*settings(ivyXML :=
-																																	<dependencies>
-																																		<exclude
-																																		module="log4j"/>
-																																		<exclude
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-																																		module="slf4j-log4j12"/>
-																																		<exclude
-																																		module="commons-logging"/>
-																																	</dependencies>)*/
-	}
-
-/*
-libraryDependencies +=  "edu.umass.cs.iesl" %% "scalacommons" % "0.1-SNAPSHOT"  changing()
-
-libraryDependencies +=  "net.liftweb" %% "lift-json" % "2.4-M5"
-
-libraryDependencies += "org.scalatest" %% "scalatest" % "1.6.1" % "test"
-
-libraryDependencies += "org.scala-tools.subcut" %% "subcut" % "1.0"
-
-addCompilerPlugin("org.scala-tools.subcut" %% "subcut" % "1.0")
-
-libraryDependencies += "com.cybozu.labs" % "langdetect" % "1.2.2"
-
-libraryDependencies += "net.arnx" % "jsonic" % "1.2.7"
-
-libraryDependencies += "org.slf4j" % "jcl-over-slf4j" % "1.6.4"
-
-libraryDependencies += "org.apache.commons" % "commons-vfs2" % "2.0" exclude("commons-logging", "commons-logging")
-
-libraryDependencies += "commons-collections" % "commons-collections" % "3.2.1"
-
-libraryDependencies += "org.apache.commons" % "commons-compress" % "1.3"
-*/
+}
--- project/project/Plugins.scala
+++ project/project/Plugins.scala
@@ -14,7 +14,7 @@ object IeslPluginLoader extends Build
 	                .settings(resolvers +=
 	                          Resolver.url("artifactory", url("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases"))(Resolver
 	                                                                                                                                 .ivyStylePatterns))
-	                .settings(addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.8.3"))
+	                .settings(addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.8.4"))
 	                .settings(addSbtPlugin("net.virtual-void" % "sbt-dependency-graph" % "0.6.0"))
 	}
 
