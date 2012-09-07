4e197113becc28f99d1ff86854efa2a51745af9f
David Soergel <dev@davidsoergel.com>
2012-09-07 12:37:23 -0400

    Remove vfs

--- src/main/scala/edu/umass/cs/iesl/bibmogrify/InputTransformers.scala
+++ src/main/scala/edu/umass/cs/iesl/bibmogrify/InputTransformers.scala
@@ -2,11 +2,7 @@ package edu.umass.cs.iesl.bibmogrify
 
 import pipeline.Transformer
 import com.weiglewilczek.slf4s.Logging
-import org.apache.commons.vfs2._
-import cache.LRUFilesCache
-import impl.{StandardFileSystemManager, DefaultFileSystemManager}
 import java.net.URL
-import sun.net.www.{MimeEntry, MimeTable}
 import org.apache.commons.compress.archivers.tar.{TarArchiveInputStream, TarArchiveEntry}
 import java.io.{ByteArrayInputStream, InputStream, File}
 import java.util.zip.{ZipEntry, ZipInputStream, GZIPInputStream}
@@ -18,55 +14,47 @@ import edu.umass.cs.iesl.scalacommons.StringUtils._
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
  * @version $Id$
  */
-object StringToUrl extends Transformer[String, URL] with NamedPlugin
-	{
-	def apply(s: String) =
-		{
+object StringToUrl extends Transformer[String, URL] with NamedPlugin {
+	def apply(s: String) = {
 		val f = new File(s)
 		if (f.exists) Some(f.toURI.toURL)
-		else if (s.contains(":"))
-			{
+		else if (s.contains(":")) {
 			Some(new URL(s))
-			}
-		else throw new BibMogrifyException("Input file not found: " + s)
 		}
-
-	val name = "toUrl"
+		else throw new BibMogrifyException("Input file not found: " + s)
 	}
 
-object AnyToString extends Transformer[Any, String] with NamedPlugin
-	{
+	val name = "toUrl"
+}
+
+object AnyToString extends Transformer[Any, String] with NamedPlugin {
 
 	val name = "toString"
 
 	def apply(v1: Any) = Some(v1.toString + "\n")
-	}
+}
 
-object LineReader extends Transformer[URL, String] with NamedPlugin
-	{
+object LineReader extends Transformer[URL, String] with NamedPlugin {
 	def apply(u: URL): TraversableOnce[String] = io.Source.fromURL(u).getLines().toStream.view
 
 	val name = "byLine"
-	}
+}
 
-object ParallelLineReader extends Transformer[URL, String] with NamedPlugin
-	{
+object ParallelLineReader extends Transformer[URL, String] with NamedPlugin {
 	def apply(u: URL): GenTraversableOnce[String] = io.Source.fromURL(u).getLines().toStream.view.par
 
 	val name = "byLinePar"
-	}
+}
 
-object UrlToStream extends Transformer[URL, NamedInputStream] with NamedPlugin
-	{
+object UrlToStream extends Transformer[URL, NamedInputStream] with NamedPlugin {
 	def apply(u: URL): TraversableOnce[NamedInputStream] = Some(new UrlNamedInputStream(u.toExternalForm.n, u))
 
 	val name = "urlToStream"
-	}
+}
 
-object Identity extends Transformer[TraversableOnce[String], String]
-	{
+object Identity extends Transformer[TraversableOnce[String], String] {
 	def apply(u: TraversableOnce[String]): TraversableOnce[String] = u
-	}
+}
 
 /*
 object VfsToInputStreams extends Transformer[String, NamedInputStream] with NamedPlugin with Logging
@@ -122,175 +110,149 @@ object VfsToInputStreams extends Transformer[String, NamedInputStream] with Name
 		}
 	}
 */
-
 /*
 case "zip" => {
   val zin = new StreamZipArchive(fileObject.getName.getBaseName, fileObject.getName.getFriendlyURI, fileObject.getContent.getInputStream);
   val result: Iterator[NamedInputStream] = zin.iterator.flatMap(e => Some(new NamedInputStream(e.name) {
-    def getInputStream = e.input
+	def getInputStream = e.input
   }))
 
   result
 }*/
-object ArchiveToInputStreams extends Transformer[URL, NamedInputStream] with NamedPlugin with Logging
-	{
+object ArchiveToInputStreams extends Transformer[URL, NamedInputStream] with NamedPlugin with Logging {
 
 	val name = "extract"
 
-	def getAllInputStreams(nis: NamedInputStream): TraversableOnce[NamedInputStream] =
-		{
+	def getAllInputStreams(nis: NamedInputStream): TraversableOnce[NamedInputStream] = {
 
 		val name = nis.name
-		val (baseName:NonemptyString, extension:NonemptyString) = name.lastIndexOf('.') match
-		{
+		val (baseName: NonemptyString, extension: NonemptyString) = name.lastIndexOf('.') match {
 			case -1 => (name, None)
 			case x: Int => (name.substring(0, x), (name.substring(x + 1).toLowerCase))
 		}
 
-		extension.s match
-		{
+		extension.s match {
 			case "tar" => processTarStream(name, nis.getInputStream).flatMap(getAllInputStreams)
 			case "xml" => Some(nis)
-			case "gz" =>
-				{
-				val x: Option[NamedInputStream] = Some(new NamedInputStream(baseName)
-					{
+			case "gz" => {
+				val x: Option[NamedInputStream] = Some(new NamedInputStream(baseName) {
 					def getInputStream = new GZIPInputStream(nis.getInputStream)
-					})
+				})
 				x.map(getAllInputStreams).getOrElse(Nil)
-				}
+			}
 			case "tgz" => processTarStream(name, new GZIPInputStream(nis.getInputStream)).flatMap(getAllInputStreams)
 			case "zip" => processZipStream(name, nis.getInputStream).flatMap(getAllInputStreams)
-			case _ =>
-				{
+			case _ => {
 				logger.warn("Unknown file extension: " + name)
 				Some(nis)
-				}
+			}
 		}
-		}
+	}
 
-	def apply(v1: URL): TraversableOnce[NamedInputStream] =
-		{
+	def apply(v1: URL): TraversableOnce[NamedInputStream] = {
 		val result = getAllInputStreams(new UrlNamedInputStream(v1.toString.n, v1))
 		//fsManager.close
 		result
-		}
+	}
 
-	private def processTarStream(baseName: String, baseStream: InputStream): Iterator[NamedInputStream]
-			{var nextEntry: Option[NamedInputStream]; def hasNext: Boolean; def readNext: Option[NamedInputStream]; def next(): NamedInputStream} =
-		{
+	private def processTarStream(baseName: String,
+	                             baseStream: InputStream): Iterator[NamedInputStream] {var nextEntry: Option[NamedInputStream]; def hasNext: Boolean; def
+	readNext: Option[NamedInputStream]; def next(): NamedInputStream} = {
 		val tarInput: TarArchiveInputStream = new TarArchiveInputStream(baseStream)
 
-		new Iterator[NamedInputStream]()
-			{
+		new Iterator[NamedInputStream]() {
 			var nextEntry: Option[NamedInputStream] = readNext
 
 			def hasNext = nextEntry.isDefined
 
 			// operating on the Java-style inputStream directly, not a Source or something,
 			// because getNextTarEntry can reposition the stream and I don't trust an intermediary
-			def readNext: Option[NamedInputStream] =
-				{
+			def readNext: Option[NamedInputStream] = {
 				val entry: TarArchiveEntry = tarInput.getNextTarEntry
 				if (entry == null) None
-				else
-					{
+				else {
 					//** todo check that the entry is an xml file (or whatever other filters are desired...)
 					val size: Int = entry.getSize.toInt // hope no entries are huge
 					// ignore anything but real files
-					if (size > 0)
-						{
+					if (size > 0) {
 						val content = new Array[Byte](size)
 						var offset = 0
-						while (offset < size)
-							{
+						while (offset < size) {
 							offset += tarInput.read(content, offset, size - offset)
-							}
+						}
 
 						// don't put the stream-reading part inside getInputStream, because there's no guarantee in what order those will be called
-						Some(new NamedInputStream((baseName + "#" + entry.getName).n)
-							{
+						Some(new NamedInputStream((baseName + "#" + entry.getName).n) {
 							def getInputStream = new ByteArrayInputStream(content)
-							})
-						}
-					else readNext
+						})
 					}
+					else readNext
 				}
+			}
 
-			def next() =
-				{
+			def next() = {
 				val result = nextEntry
 				nextEntry = readNext
 				result.get // error if next was called when hasNext == false
-				}
 			}
 		}
+	}
 
-	private def processZipStream(baseName: String, baseStream: InputStream): Iterator[NamedInputStream]
-			{var nextEntry: Option[NamedInputStream]; def hasNext: Boolean; def readNext: Option[NamedInputStream]; def next(): NamedInputStream} =
-		{
+	private def processZipStream(baseName: String,
+	                             baseStream: InputStream): Iterator[NamedInputStream] {var nextEntry: Option[NamedInputStream]; def hasNext: Boolean; def
+	readNext: Option[NamedInputStream]; def next(): NamedInputStream} = {
 		val zipInput: ZipInputStream = new ZipInputStream(baseStream)
 
-		new Iterator[NamedInputStream]()
-			{
+		new Iterator[NamedInputStream]() {
 			var nextEntry: Option[NamedInputStream] = readNext
 
 			def hasNext = nextEntry.isDefined
 
 			// operating on the Java-style inputStream directly, not a Source or something,
 			// because getNextTarEntry can reposition the stream and I don't trust an intermediary
-			def readNext: Option[NamedInputStream] =
-				{
+			def readNext: Option[NamedInputStream] = {
 				val entry: ZipEntry = zipInput.getNextEntry
 				if (entry == null) None
-				else
-					{
+				else {
 					//** todo check that the entry is an xml file (or whatever other filters are desired...)
 					val size: Int = entry.getSize.toInt // hope no entries are huge
 					// ignore anything but real files
-					if (size > 0)
-						{
+					if (size > 0) {
 						val content = new Array[Byte](size)
 						var offset = 0
-						while (offset < size)
-							{
+						while (offset < size) {
 							offset += zipInput.read(content, offset, size - offset)
-							}
+						}
 
 						// don't put the stream-reading part inside getInputStream, because there's no guarantee in what order those will be called
-						Some(new NamedInputStream((baseName + "#" + entry.getName).n)
-							{
+						Some(new NamedInputStream((baseName + "#" + entry.getName).n) {
 							def getInputStream = new ByteArrayInputStream(content)
-							})
-						}
-					else readNext
+						})
 					}
+					else readNext
 				}
+			}
 
-			def next() =
-				{
+			def next() = {
 				val result = nextEntry
 				nextEntry = readNext
 				result.get // error if next was called when hasNext == false
-				}
 			}
 		}
 	}
+}
 
-abstract class NamedInputStream(val name: NonemptyString)
-	{
+abstract class NamedInputStream(val name: NonemptyString) {
 	def getInputStream: InputStream
-	}
-
-class VfsNamedInputStream(name: NonemptyString, content: FileContent) extends NamedInputStream(name)
-	{
+}
+/*
+class VfsNamedInputStream(name: NonemptyString, content: FileContent) extends NamedInputStream(name) {
 	def getInputStream = content.getInputStream
-	}
-
-class UrlNamedInputStream(name: NonemptyString, content: URL) extends NamedInputStream(name)
-	{
+}
+*/
+class UrlNamedInputStream(name: NonemptyString, content: URL) extends NamedInputStream(name) {
 	def getInputStream = content.openStream()
-	}
+}
 
 /*
 object temp {
