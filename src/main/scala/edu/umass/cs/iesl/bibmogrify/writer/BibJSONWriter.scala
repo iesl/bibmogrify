e5955e9a45e93d25a7a2f1108805ed851751a9b2
David Soergel <dev@davidsoergel.com>
2012-10-05 12:31:39 -0400

    expand bibjson output

--- src/main/scala/edu/umass/cs/iesl/bibmogrify/writer/BibJSONWriter.scala
+++ src/main/scala/edu/umass/cs/iesl/bibmogrify/writer/BibJSONWriter.scala
@@ -1,6 +1,6 @@
 package edu.umass.cs.iesl.bibmogrify.writer
 
-import edu.umass.cs.iesl.bibmogrify.model.StructuredCitation
+import edu.umass.cs.iesl.bibmogrify.model.{AuthorInRole, StructuredCitation}
 import net.liftweb.json.JsonAST._
 import net.liftweb.json.Extraction._
 import net.liftweb.json.Printer._
@@ -21,15 +21,13 @@ object BibJSONWriter extends Transformer[StructuredCitation, String] with NamedP
       val qq: List[Option[(String, Object)]] = List(
         cm.title.map(n => "title" -> n.s),
         cm.englishAbstract.headOption.map("abstract" -> _),
-        cm.containedIn.headOption.flatMap(_.container.title.map("journal" -> _.toString)),
+        cm.containedIn.headOption.flatMap(_.container.title.map(j => "journal" -> Map("name" ->j.toString))),
         //cm.abstractText.map("abstract" -> _),
         cm.dates.headOption.flatMap(_.date.flatMap(_.year.map("year" -> _.toString))),
-        Some("authors" -> {
-          val q = cm.authors map {
-            a => Map(("name" -> a.agent.toString))
-          }
-          q
-        }))
+        Some("author" -> (cm.authors map {a => Map(("name" -> a.agent.toString))})),
+        Some("link" -> (cm.locations map {a => Map(("url" -> a.toString))})),  // for now just call everything a "url" even if it isn't
+        Some("identifier" -> (cm.identifiers map {a => Map(("type" -> a.authority.toString),("id" -> a.value))}))
+      )
       val m: Map[String, Object] = qq.flatten.toMap
       //"citationmention" -> m
       m
